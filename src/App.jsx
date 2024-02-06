16868052df64411063503cf40be7aeaa185c7e02
Sara Alhaddadi <sara.khalid.alhaddadi@gmail.com>
2024-02-06 22:30:01 +0300

    finial fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
-
 const server = "localhost:3000";
 const webSocket = new WebSocket(`ws://${server}/cable`);
 const messagesContainer = document.getElementById("messages");
+
 function App() {
   const [messages, setMessages] = useState([]);
   const [guid, setGuid] = useState("");
@@ -16,8 +14,11 @@ function App() {
 
     webSocket.send(
       JSON.stringify({
-        id: guid,
-        channel: "MessagesChannel",
+        command: "subscribe",
+        identifier: JSON.stringify({
+          id: guid,
+          channel: "MessagesChannel",
+        }),
       })
     );
   };
@@ -25,11 +26,10 @@ function App() {
   webSocket.onmessage = (e) => {
     const data = JSON.parse(e.data);
     console.log(`get message webSocket server ${server} which ${data.type}`);
-    console.log(data);
 
-    if (data.type === 'ping') return;
-    if (data.type === 'welcome') return;
-    if (data.type === 'confirm_submit') return;
+    if (data.type === "ping") return;
+    if (data.type === "welcome") return;
+    if (data.type === "confirm_subscription") return;
 
     const message = data.message;
     setMessagesAndScrollDown([...messages, message]);
@@ -39,17 +39,9 @@ function App() {
     fetchMessages();
   }, []);
 
-  const fetchMessages = async () => {
-    const response = await fetch(`http://${server}/messages`);
-    const data = await response.json();
-    setMessagesAndScrollDown(data);
-  };
-
-  const setMessagesAndScrollDown = (data) => {
-    setMessages(data);
-    if (!messagesContainer) return;
-    messagesContainer.scrollTop = messagesContainer.scrollHeight;
-  };
+  useEffect(() => {
+    resetScroll();
+  }, [messages]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -65,10 +57,26 @@ function App() {
     });
   };
 
+  const fetchMessages = async () => {
+    const response = await fetch("http://localhost:3000/messages");
+    const data = await response.json();
+    setMessagesAndScrollDown(data);
+  };
+
+  const setMessagesAndScrollDown = (data) => {
+    setMessages(data);
+    resetScroll();
+  };
+
+  const resetScroll = () => {
+    if (!messagesContainer) return;
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  };
+
   return (
     <div className="App">
       <div className="messageHeader">
-        <h2>Messages</h2>
+        <h1>Messages</h1>
         <p>Guid: {guid}</p>
       </div>
 
